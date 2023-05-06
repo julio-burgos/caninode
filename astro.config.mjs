dd4e51c8c4768a049b84987075e65b2d21754b03
julio.burgos <julio.burgos@dnv.com>
2023-05-06 13:18:23 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
+import prefetch from '@astrojs/prefetch';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://caninode.pages.dev",
+  integrations: [tailwind(), prefetch(), sitemap()],
+});
